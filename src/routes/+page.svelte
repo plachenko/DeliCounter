6b854ae6833ff13f85de5272d6618f7ef3926805
Denis Perchenko <plachenko@gmail.com>
2024-10-21 14:46:50 -0400

    Adding tailwind and basic layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,39 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div id="Container">
+	<div class="w-full h-[50px] bg-slate-400"></div>
+	<div class="flex-1 w-full bg-slate-700"></div>
+	<div class="w-full h-[50px] bg-slate-300"></div>
+</div>
+
+<style>
+	.sections {
+		display: flex;
+	}
+
+	#Container {
+		height: 100%;
+		width: 100%;
+		position: absolute;
+		left: 0px;
+		top: 0px;
+		display: flex;
+		flex-direction: column;
+	}
+
+	#Header {
+		height: 40px;
+		width: 100%;
+		background-color: #fff;
+		border-bottom: 1px solid;
+	}
+
+	#Body {
+		flex: 1;
+		height: 100%;
+		background-color: #f0f;
+	}
+
+	#Footer {
+		background-color: #00f;
+		border-top: 1px solid;
+	}
+</style>
