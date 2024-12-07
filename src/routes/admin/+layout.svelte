f94585f7f95586639b578b105135cfc9fa12f7f4
Denis Perchenko <plachenko@gmail.com>
2024-12-06 23:50:04 -0500

    adding admin item panel

---
+++ src/routes/admin/+layout.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { onMount } from "svelte";
+
+    let current = $state(null);
+
+    let links = [
+        { name: 'Dashboard', link: '/admin' },
+        { name: 'Items', link: '/admin/add' },
+    ];
+
+    onMount(() => {
+        current = links[0];
+    });
+
+</script>
+
+<div class="w-full h-full flex flex-col bg-slate-100">
+    <div class="flex gap-2 bg-slate-400 border-b-2 w-full">
+        {#each links as link}
+        <a aria-label={link.name} onclick={() => { current = link; }} href={link.link} class={`text-black p-2 hover:bg-slate-600 ${current?.name == link.name ? 'border-slate-600 border-b-2 font-bold' : ''}`}>{link.name}</a>
+        {/each}
+    </div>
+
+    <slot />
+</div>
---
+++ src/routes/admin/add/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+    let items = $state(null);
+    let inputTxt = $state(null);
+
+
+</script>
+<div class="w-full h-[50px] bg-red-400 p-2">
+    <div class="w-full h-full bg-slate-400">
+
+    </div>
+    <div class="w-full h-[20px] bg-slate-500">
+
+    </div>
+    <!--
+    <div class="w-full h-full flex p-2">
+    <div class="w-full h-full flex gap-2">
+        <div class="flex-1 w-full h-full bg-white rounded-md flex items-center justify-center p-1">
+    
+            <div contenteditable="true" bind:this={inputTxt} class="flex-1 w-full h-full bg-white"></div>
+        </div>
+        <button onclick={() => {items.push({name: 'test'})}} class="h-full bg-slate-200 rounded-md font-bold px-3">new item</button>
+    </div>
+    <div class="w-full h-[20px] bg-slate-900">
+
+    </div>
+</div>
+    -->
+
+</div>
